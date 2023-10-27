942381f999bf8835b0ae6c55d064316783c5f433
azolfagharir <azolfagharir@gmail.com>
2023-10-27 12:48:45 +0330

    i changed the port to 3001

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
-const PORT  = 3000;
+const PORT  = 3001;
+
+app.get('/',(req, res)=>{
+    res.send('hello world');
+})
 app.listen(PORT, ()=>{
     console.log('App listening on port '+ PORT);
 
