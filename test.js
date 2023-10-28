3237ac76eb6c3193c83980bcb2a3a15b17b6c937
azolfagharir <azolfagharir@gmail.com>
2023-10-28 20:49:27 +0330

    continuing the tutorial

--- test.js
+++ test.js
@@ -2,9 +2,29 @@ const express = require('express');
 const app = express();
 const PORT  = 3001;
 
-app.get('/',(req, res)=>{
-    res.send('hello world');
+
+const json = {
+    "name": "amir",
+    "ftwr": "ke",
+    "favorite_colors": [
+        {
+            "my_favorite_color": "blue",
+            "his_favorite_color": "red"
+        }
+    ]
+};
+
+app.get('/costumers/data',(req, res)=>{
+    res.send({"my data" : json});
 })
+app.post('/api/costumers', (req, res)=>{
+    console.log(req.body);
+    res.send(req.body);
+});
+
+app.post('/api/costumers', (req, res)=>{
+    res.send('thos is a post request');
+});
 app.listen(PORT, ()=>{
     console.log('App listening on port '+ PORT);
 
